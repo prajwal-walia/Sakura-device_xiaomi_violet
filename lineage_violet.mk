d796da5c76372b2190717f3d5946d9501feb3bbb
prajwal-walia <prajwalahluwalia5@gmail.com>
2020-06-30 13:52:50 +0000

    Sakura Bringup

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -13,20 +13,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common Pixys stuff.
+# Inherit some common Sakura stuff.
 OVERRIDE_TARGET_FLATTEN_APEX := false
 TARGET_BOOT_ANIMATION_RES := 1080
+SAKURA_GAPPS=true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # MIUI Camera
 $(call inherit-product-if-exist, vendor/MiuiCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixys_violet
+PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
