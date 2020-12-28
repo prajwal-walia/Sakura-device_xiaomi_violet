fd4c7b0cea1118ac8cf1beba103751682ced2dc1
funeralfrog <nitish.98.sharma@gmail.com>
2020-12-28 20:23:03 +0000

    violet: Lets do gapps

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -11,6 +11,7 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common Project Sakura stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 SAKURA_OFFICIAL := true
+SAKURA_GAPPS := true
 SAKURA_MAINTAINER := Prajwal&&Nitish
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
