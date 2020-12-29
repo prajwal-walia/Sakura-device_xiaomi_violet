5cf4e38f934a0d3dd44c1d76f8607032cdc0d5f5
funeralfrog <nitish.98.sharma@gmail.com>
2020-12-29 08:26:28 +0000

    violet: Recover removed overlays
    and fixing a derp

--- device.mk
+++ device.mk
@@ -22,10 +22,11 @@ $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Properties
