c70734bb39cd571298be7aa5e7846d724278ce20
funeralfrog <nitish.98.sharma@gmail.com>
2020-12-29 10:04:13 +0000

    violet: build trust hal

--- device.mk
+++ device.mk
@@ -409,6 +409,10 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     telephony-ext
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
